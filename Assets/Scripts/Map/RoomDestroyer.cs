6b7f25a707ad889c3a2bd4596fc0c0c11eb65d2d
Đoàn Việt Vương <doanvietvuong2807@gmail.com>
2024-07-14 22:52:51 +0700

    addBorder

--- Assets/Scripts/Map/RoomDestroyer.cs
+++ Assets/Scripts/Map/RoomDestroyer.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class RoomDestroyer : MonoBehaviour
 {
+    [SerializeField]
+    GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(Player, transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(collision.gameObject);
+        if (collision.CompareTag("SpawnPoint") || collision.CompareTag("Map"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
+
 }
