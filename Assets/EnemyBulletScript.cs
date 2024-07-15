2213640ccc8b0b607497d9756ad3597b68f4ab98
dungnthe163811 <dungnthe163811@fpt.edu.vn>
2024-07-15 13:16:44 +0700

    [ADD]/ Enemy-Skeleton Shooting, Range shooting

---
+++ Assets/EnemyBulletScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletScript : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    public float force;
+    private float timer;
+    [SerializeField]
+    public float attackDamage = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 180);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 10)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().health -= attackDamage;
+        }
+    }
+}
