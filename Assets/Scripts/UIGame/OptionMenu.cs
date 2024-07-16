f0e1880cb48fc6a086d1c1c6f5e24c194ea67c78
Nguyễn Thành Thao <97202947+thaonthe151260@users.noreply.github.com>
2024-07-16 22:32:45 +0700

    Update OptionMenu.cs

--- Assets/Scripts/UIGame/OptionMenu.cs
+++ Assets/Scripts/UIGame/OptionMenu.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
 
-    public void SetVolume(float volume)
+    public void SetMusicVolume()
     {
-        audioMixer.SetFloat("volume", volume);
+        float musicVolume = musicSlider.value;
+        audioMixer.SetFloat("volume", Mathf.Log10(musicVolume) *20);
+    }
+
+    public void SetSfxVolume()
+    {
+        float sfxVolume = sfxSlider.value;
+        audioMixer.SetFloat("sfx", Mathf.Log10(sfxVolume) * 20);
     }
 
     public void SetQuality(int qualityIndex)
